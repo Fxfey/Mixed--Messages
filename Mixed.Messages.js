97e61e39d8f46f80b0aac973b3b0e45469921495
[Ben Fyfe] <benfyfe1999@gmail.com>
2021-04-08 12:32:28 +0100

    Added a random number generator to allow for a random number of minutes

--- Mixed.Messages.js
+++ Mixed.Messages.js
@@ -1,10 +1,14 @@
 // Random gaming warmup generator.
 let message1 = ['Movement','Aim','Game Sense'];
 let message2 = ['Positioning','Utility Practice','Teamwork']
-let message3 = ['10 Minutes','15 Minutes','20 Minutes','30 Minutes','45 Minutes','1 Hour'];
+let minute = 0;
 // The code below returns a random value of the array put into the parameter.
 const randomValue = (list) => {
     return list[Math.floor(Math.random() * list.length)];
 };
+// Creates a random number to dsetermine how long to do that specific task for.
+const randomNumber = (input) => {
+    return [Math.floor(Math.random() * 60)]
+};
 
-console.log(`You will practice ${randomValue(message1)} for ${randomValue(message3)} and ${randomValue(message2)} for ${randomValue(message3)}`);
+console.log(`You will practice ${randomValue(message1)} for ${randomNumber(minute)} minutes and ${randomValue(message2)} for ${randomNumber(minute)} minutes`);
