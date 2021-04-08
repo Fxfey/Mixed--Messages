cd67add98a02e51ad41a65c69408523408d23bda
[Ben Fyfe] <benfyfe1999@gmail.com>
2021-04-08 12:22:03 +0100

    Added a new array for tasks to prevent duplicate tasks being made

--- Mixed.Messages.js
+++ Mixed.Messages.js
@@ -1,9 +1,10 @@
 // Random gaming warmup generator.
-let message1 = ['Movement','Aim','Game Sense','Positioning','Utility Practice','Teamwork'];
-let message2 = ['10 Minutes','15 Minutes','20 Minutes','30 Minutes','45 Minutes','1 Hour'];
+let message1 = ['Movement','Aim','Game Sense'];
+let message2 = ['Positioning','Utility Practice','Teamwork']
+let message3 = ['10 Minutes','15 Minutes','20 Minutes','30 Minutes','45 Minutes','1 Hour'];
 // The code below returns a random value of the array put into the parameter.
 const randomValue = (list) => {
     return list[Math.floor(Math.random() * list.length)];
 };
 
-console.log(`You will practice ${randomValue(message1)} for ${randomValue(message2)} and ${randomValue(message1)} for ${randomValue(message2)}`);
+console.log(`You will practice ${randomValue(message1)} for ${randomValue(message3)} and ${randomValue(message2)} for ${randomValue(message3)}`);
